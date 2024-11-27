91eacc30b7ed00a23589d953ef727142c508f965
UniverseVG <gmvarun2001@gmail.com>
2024-11-27 22:11:43 +0530

    fix the doctor edit modal

--- components/forms/DoctorForm.tsx
+++ components/forms/DoctorForm.tsx
@@ -135,7 +135,7 @@ const DoctorForm = ({ type, doctor, setOpen }: DoctorFormProps) => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className={`${isEdit ? "space-y-6" : "space-y-12"}`}
+        className={`flex-1 ${isEdit ? "space-y-6" : "space-y-12"}`}
       >
         {!isEdit && (
           <section className="space-y-4">
